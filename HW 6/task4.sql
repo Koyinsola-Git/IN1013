229d8e7fd03f3f6e8e9683b6af26a05fbe4c08b8
Koyinsola-Git <koyinsola-oshodi.oshodi@city.ac.uk>
2024-12-02 00:15:41 +0000

    Updated task4.sql

--- HW 6/task4.sql
+++ HW 6/task4.sql
@@ -1,53 +1,57 @@
-
--- 1
-SELECT first_name FROM restBill
-INNER JOIN restStaff
-ON restStaff.staff_no = restBill.waiter_no
-WHERE cust_name = "Tanya Singh";
+                    
+-- 1                     
+SELECT DISTINCT bills.cust_name 
+FROM restBill bills
+WHERE bills.bill_total > 450.0 
+AND bills.table_no IN (
+  SELECT table_no
+  FROM restRoom_management
+  WHERE headwaiter IN (
+    SELECT staff_no
+    FROM restStaff
+    WHERE first_name = "Charles"
+  )
+);
 
 -- 2
-SELECT room_date FROM restRoom_management
-INNER JOIN restStaff
-ON restStaff.staff_no = restRoom_management.headwaiter
-AND restStaff.first_name = "Charles"
-WHERE room_date BETWEEN 160200 AND 160231
-AND room_name="Green";
+SELECT DISTINCT(first_name)
+FROM restStaff
+WHERE staff_no IN (
+  SELECT headwaiter
+  FROM restStaff
+  WHERE staff_no IN (
+    SELECT waiter_no
+    FROM restBill
+    WHERE bill_date = 160111 AND cust_name LIKE "Nerida%"
+  )
+);
 
 -- 3
-SELECT T1.first_name,T1.surname 
-FROM restStaff T1
-INNER JOIN restStaff T2
-INNER JOIN restStaff T3
-ON T3.first_name = "Zoe" AND T3.surname = "Ball" 
-AND T1.headwaiter = T2.staff_no AND T3.headwaiter = T2.staff_no; 
+SELECT cust_name
+FROM restBill
+WHERE bill_total IN (
+  SELECT MIN(bill_total)
+  FROM restBill
+);
 
 -- 4
-SELECT cust_name,bill_total,first_name
-FROM restBill
-INNER JOIN restStaff
-ON staff_no = waiter_no
-ORDER BY bill_total;
-
+SELECT first_name
+FROM restStaff
+WHERE staff_no NOT IN (
+  SELECT waiter_no
+  FROM restBill
+);
 
 -- 5
-SELECT S1.first_name,S1.surname 
-FROM restStaff S1
-INNER JOIN restStaff S2
-INNER JOIN restBill Bills
-ON S1.headwaiter = S2.headwaiter
-AND Bills.waiter_no = S2.staff_no
-WHERE Bills.bill_no = 0014 OR Bills.bill_no = 0017;
-
-
--- 6
-SELECT S1.first_name,S1.surname
-FROM restStaff S1
-INNER JOIN restStaff S2
-INNER JOIN restBill bills
+SELECT DISTINCT cust_name,staff.first_name,staff.surname,rooms.room_name
+FROM restBill
+INNER JOIN restStaff staff
 INNER JOIN restRest_table tables
-ON bills.waiter_no = S2.staff_no 
-AND S1.headwaiter = S2.headwaiter
-AND tables.table_no = bills.table_no
-WHERE bills.bill_date = 160312
-AND tables.room_name = "Blue";
-
+INNER JOIN restRoom_management rooms
+ON restBill.table_no = tables.table_no
+AND rooms.room_name = tables.room_name
+AND rooms.headwaiter = staff.staff_no
+WHERE bill_total IN (
+  SELECT max(bill_total)
+  FROM restBill
+);
